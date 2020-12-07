ba99024b45de799c007f5852027192cc4561e1a0
krystek77 <krystian.wrona77@gmail.com>
2020-12-07 09:37:20 +0100

    Change default config to get business by slug

--- api/business/controllers/business.js
+++ api/business/controllers/business.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.business.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.business });
+  },
+};
