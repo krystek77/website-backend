c1ade5a1f8e3015c92f9465fe6807defa14f0558
krystek77 <krystian.wrona77@gmail.com>
2020-12-09 14:23:45 +0100

    Update strapi data sctructure

---
+++ api/lines/controllers/lines.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.lines.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.lines });
+  },
+};
---
+++ api/product/controllers/product.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {};
+
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.services.product.findOne({ slug });
+    return sanitizeEntity(entity, { model: strapi.models.product });
+  },
+};
